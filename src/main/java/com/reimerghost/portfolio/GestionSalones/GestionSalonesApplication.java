899d5480a2aeda67ac95e253de05f1e626adebe3
ReimerGhost <reimerghost@gmail.com>
2025-02-03 19:14:30 -0600

    Inicializando Proyecto

---
+++ src/main/java/com/reimerghost/portfolio/GestionSalones/GestionSalonesApplication.java
@@ -0,0 +1,13 @@
+package com.reimerghost.portfolio.GestionSalones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSalonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSalonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reimerghost/portfolio/GestionSalones/GestionSalonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reimerghost.portfolio.GestionSalones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSalonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
